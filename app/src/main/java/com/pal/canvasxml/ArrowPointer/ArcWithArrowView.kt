af195cc1090e71c8aa74955e67456b44f55b537f
palpa <payel_paul@msrlimited.com>
2023-07-31 20:56:54 +0530

    triangle shape

--- app/src/main/java/com/pal/canvasxml/ArrowPointer/ArcWithArrowView.kt
+++ app/src/main/java/com/pal/canvasxml/ArrowPointer/ArcWithArrowView.kt
@@ -22,8 +22,8 @@ class ArcWithArrowView @JvmOverloads constructor(
 
     private val arrowPaint = Paint().apply {
         color = Color.RED
-        style = Paint.Style.FILL
-        isAntiAlias = false
+        strokeWidth = 4f // Adjust the width of the arrowhead outline as needed
+        isAntiAlias = true
     }
 
     private val startAngle = -180f
@@ -34,12 +34,12 @@ class ArcWithArrowView @JvmOverloads constructor(
 
         val centerX = width / 2f
         val centerY = height / 2f
-        val radius = (Math.min(centerX, centerY) * 0.8).toFloat()
+        val radius = (centerX.coerceAtMost(centerY) * 0.8).toFloat()
 
         // Calculate the start and end points of the arc
         val startX = centerX + radius * cos(Math.toRadians(startAngle.toDouble())).toFloat()
         val startY = centerY + radius * sin(Math.toRadians(startAngle.toDouble())).toFloat()
-        val endX = centerX + radius * cos(Math.toRadians((startAngle + sweepAngle).toDouble())).toFloat()
+        val endX = centerX  + radius * cos(Math.toRadians((startAngle + sweepAngle).toDouble())).toFloat()
         val endY = centerY + radius * sin(Math.toRadians((startAngle + sweepAngle).toDouble())).toFloat()
 
         // Draw the arc
@@ -49,27 +49,31 @@ class ArcWithArrowView @JvmOverloads constructor(
             centerX + radius,
             centerY + radius,
             startAngle,
-            sweepAngle,
+            180f,
             false,
             arcPaint
+
         )
 
         // Calculate the coordinates of the arrowhead
-        val arrowSize = 100f // Modify the size of the arrowhead as needed
-        val arrowEndPointX = endX + arrowSize * cos(Math.toRadians((startAngle + sweepAngle - 20).toDouble())).toFloat()
-        val arrowEndPointY = endY + arrowSize * sin(Math.toRadians((startAngle + sweepAngle - 20).toDouble())).toFloat()
+        val arrowSize = 120f // Modify the size of the arrowhead as needed
+        val arrowEndPointX = endX + arrowSize * cos(Math.toRadians((startAngle + sweepAngle + 180).toDouble())).toFloat()
+        val arrowEndPointY = endY + arrowSize * sin(Math.toRadians((startAngle + sweepAngle + 180).toDouble())).toFloat()
 
-        // Draw the arrowhead
+        // Draw the arrowhead outline
         val path = Path().apply {
-            moveTo(endX, endY)
+            moveTo(endX , endY )
             lineTo(arrowEndPointX, arrowEndPointY)
             lineTo(
-                endX + arrowSize * cos(Math.toRadians((startAngle + sweepAngle + 20).toDouble())).toFloat(),
-                endY + arrowSize * sin(Math.toRadians((startAngle + sweepAngle + 20).toDouble())).toFloat()
+                endX + arrowSize * cos(Math.toRadians((startAngle + sweepAngle + 140).toDouble())).toFloat(),
+                endY + arrowSize * sin(Math.toRadians((startAngle + sweepAngle + 140).toDouble())).toFloat()
             )
             close()
         }
         canvas.drawPath(path, arrowPaint)
     }
 }
+
+
+
 
